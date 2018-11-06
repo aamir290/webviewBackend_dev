f121cb3c3d9c24f48ed08b26e62e3c6285e4f3bf
Jeremy Le Piolet <jeremy.le-piolet@gfi.fr>
2018-11-06 17:28:37 +0100

    Add - Server class for handling server configuration and launch

---
+++ src/infrastructure/Server.js
@@ -0,0 +1,19 @@
+/**
+ * Server Class.
+ * Setup server and launch.
+ */
+
+module.exports = class Server {
+
+  /**
+   * Init application.
+   */
+  constructor() {
+  }
+
+  async start() {
+    return new Promise((resolve) => {
+      resolve();
+    });
+  }
+};

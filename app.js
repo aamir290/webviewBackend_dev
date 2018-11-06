7979cdff04160e299331eb95029ac5c74531dcd0
Jeremy Le Piolet <jeremy.le-piolet@gfi.fr>
2018-11-06 14:10:06 +0100

    Change - prepare directory structure

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var express = require('express');
 var path = require('path');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
+var indexRouter = require('./src/presentation/routes/index');
 
 var app = express();
 

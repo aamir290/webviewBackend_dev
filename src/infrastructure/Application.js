55f02eba8c2952b8b133ccfc99f51313e8a0703b
Jeremy Le Piolet <jeremy.le-piolet@gfi.fr>
2018-11-06 17:25:32 +0100

    Change - Application class

--- src/infrastructure/Application.js
+++ src/infrastructure/Application.js
@@ -3,6 +3,19 @@
  * Initiate global element and start server.
  */
 
-class Application {
+module.exports = class Application {
 
-}
+  /**
+   * Init application.
+   * @param server representing server object to start for app.
+   */
+  constructor(server) {
+    this.server = server;
+  }
+
+  async start() {
+    if (this.server.start) {
+      await this.server.start();
+    }
+  }
+};

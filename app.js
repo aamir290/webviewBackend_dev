4d9fbc1ab14569b48d159f51c2b9c8ba9b296f26
Jeremy Le Piolet <jeremy.le-piolet@gfi.fr>
2018-11-06 14:28:57 +0100

    Remove - remove view engine setup

--- app.js
+++ app.js
@@ -13,9 +13,6 @@ var indexRouter = require('./src/presentation/routes/index');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));

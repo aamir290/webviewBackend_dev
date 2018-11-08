1788cfee282f023f6904f5056d5ceb0cb0114bfe
Jeremy Le Piolet <jeremy.le-piolet@gfi.fr>
2018-11-08 15:59:01 +0100

    Add - max size for log file

--- src/infrastructure/logging/logger.js
+++ src/infrastructure/logging/logger.js
@@ -32,18 +32,27 @@ module.exports = (config) => {
   const logger = winston.createLogger({
     level: config.log.level || 'info',
     format: winston.format.combine(
-      winston.format.splat(),
-      winston.format.timestamp(),
-      winston.format.prettyPrint(),
-      myFormat
+      winston.format.splat(), //can use %s, %o and others in log
+      winston.format.timestamp(), //add timestamp to log
+      winston.format.prettyPrint(), //pretty print
+      myFormat  //custom format
     ),
     transports: [
       //
       // - Write to all logs with level `info` and below to `combined.log`
       // - Write all logs error (and below) to `error.log`.
       //
-      new winston.transports.File({filename: path.join(logDirectory, 'error.log'), level: 'error'}),
-      new winston.transports.File({filename: path.join(logDirectory, 'combined.log')})
+      new winston.transports.File({
+        filename: path.join(logDirectory, 'error.log'),
+        level: 'error',
+        maxFiles: 1,
+        maxsize: 10485760 //max size for log : 10M
+      }),
+      new winston.transports.File({
+        filename: path.join(logDirectory, 'combined.log'),
+        maxFiles: 1,
+        maxsize: 10485760 //max size for log : 10M
+      })
     ]
   });
 
@@ -55,7 +64,7 @@ module.exports = (config) => {
       format: winston.format.combine(
         winston.format.splat(),
         winston.format.timestamp(),
-        winston.format.colorize(),
+        winston.format.colorize(),  //colorize log in console
         winston.format.prettyPrint(),
         myFormat
       )

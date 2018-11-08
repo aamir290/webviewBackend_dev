8799496e9681d9e2ce1475d55d2bf776489c6152
Jeremy Le Piolet <jeremy.le-piolet@gfi.fr>
2018-11-08 16:52:44 +0100

    Change - test on app.js - not relevant

--- app.js
+++ app.js
@@ -7,19 +7,21 @@
 //Includes middleware
 var express = require('express');
 var path = require('path');
-const loggerMiddleware = require('./src/infrastructure/logging/requestLoggerToFile');
+// const loggerMiddleware = require('./src/infrastructure/logging/requestLoggerToFile');
 
 //Router
-var indexRouter = require('./src/infrastructure/routes/index');
+// var indexRouter = require('./src/infrastructure/routes/index');
 
 //Configure application object
 var app = express();
 
-app.use(loggerMiddleware());
+// app.use(loggerMiddleware());
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/', function (req, res, next) {
+  res.send('respond with a resource');
+});
 
 module.exports = app;

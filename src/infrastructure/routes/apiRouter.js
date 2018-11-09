be4b6b8724fb3213c8718b74e9c72ca0f903d238
Jeremy Le Piolet <jeremy.le-piolet@gfi.fr>
2018-11-09 14:46:23 +0100

    Change - create class for apiRouter encapsulation

--- src/infrastructure/routes/apiRouter.js
+++ src/infrastructure/routes/apiRouter.js
@@ -3,7 +3,6 @@
  */
 
 const express = require('express');
-const path = require('path');
 const Status = require('http-status');
 
 /**
@@ -11,25 +10,50 @@ const Status = require('http-status');
  * @param useCaseContainer container that contains usecase
  * @returns {*} api router
  */
-module.exports = (useCaseContainer) => {
+class ApiRouter {
 
-  //Init router
-  const appRouter = express.Router();
+  constructor(useCaseContainer){
+    this.useCaseContainer = useCaseContainer;
+    this.apiRouter = express.Router();
 
-  //Init route
-  appRouter.get('/api', function (req, res, next) {
-    if (useCaseContainer && useCaseContainer.getAllCategoriesUsecase) {
-      const getAllCategoriesUseCase = new useCaseContainer.getAllCategoriesUsecase();
-      const {SUCCESS, ERROR} = getAllCategoriesUseCase.events;
+    //Init route
+    this.apiRouter.get('/api', function (req, res, next) {
+      if (useCaseContainer && useCaseContainer.getAllCategoriesUsecase) {
+        const getAllCategoriesUseCase = new useCaseContainer.getAllCategoriesUsecase();
+        const {SUCCESS, ERROR} = getAllCategoriesUseCase.events;
 
-      getAllCategoriesUseCase.on(SUCCESS, () => {
-        console.log("OK event");
-        res.status(Status.OK).send('Success API');
-      });
-      getAllCategoriesUseCase.execute();
-    }
-  });
+        getAllCategoriesUseCase.on(SUCCESS, () => {
+          res.status(Status.OK).send('Success API');
+        });
+        getAllCategoriesUseCase.execute();
+      }
+    });
+  }
 
 
-  return appRouter;
-};
+
+}
+
+module.exports = ApiRouter;
+
+// module.exports = (useCaseContainer) => {
+//
+//   //Init router
+//   const appRouter = express.Router();
+//
+//   //Init route
+//   appRouter.get('/api', function (req, res, next) {
+//     if (useCaseContainer && useCaseContainer.getAllCategoriesUsecase) {
+//       const getAllCategoriesUseCase = new useCaseContainer.getAllCategoriesUsecase();
+//       const {SUCCESS, ERROR} = getAllCategoriesUseCase.events;
+//
+//       getAllCategoriesUseCase.on(SUCCESS, () => {
+//         res.status(Status.OK).send('Success API');
+//       });
+//       getAllCategoriesUseCase.execute();
+//     }
+//   });
+//
+//
+//   return appRouter;
+// };

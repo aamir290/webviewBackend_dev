13937a948887bbebbcae7661b89d77059654cf3f
Jeremy Le Piolet <jeremy.le-piolet@gfi.fr>
2018-11-12 10:20:34 +0100

    Add - test for getting success on getAllCategoriesUseCase

--- test/unit/usecases/GetAllCategoriesUseCaseTest.spec.js
+++ test/unit/usecases/GetAllCategoriesUseCaseTest.spec.js
@@ -2,12 +2,19 @@
  * Unit tests for GetAllCategoriesUseCase
  */
 const { expect } = require('chai');
+const GetAllCategoriesUseCase = require('../../../src/usecases/GetAllCategoriesUseCase');
 
 describe('GetAllCategoriesUseCase', () => {
 
-  context('when usecase is called', () => {
-    it('works', ()=>{
-      expect('ok').to.be.equal('ok');
+  context('when query is successful', () => {
+    it('emits SUCCESS', (done)=>{
+      const getAllCategories = new GetAllCategoriesUseCase();
+
+      getAllCategories.on(getAllCategories.events.SUCCESS, () => {
+        done();
+      });
+
+      getAllCategories.execute();
     });
   });
 });

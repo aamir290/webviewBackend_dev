f61a8b9c95a7965c1758802374f0d0b725743a4d
Jeremy Le Piolet <jeremy.le-piolet@gfi.fr>
2018-11-12 09:52:35 +0100

    Add - tasks and configuration for unit-testing

---
+++ test/setup.js
@@ -0,0 +1,5 @@
+/**
+ * Common setup for tests
+ */
+
+
---
+++ test/unit/usecases/GetAllCategoriesUseCaseTest.spec.js
@@ -0,0 +1,13 @@
+/**
+ * Unit tests for GetAllCategoriesUseCase
+ */
+const { expect } = require('chai');
+
+describe('GetAllCategoriesUseCase', () => {
+
+  context('when usecase is called', () => {
+    it('works', ()=>{
+      expect('ok').to.be.equal('ok');
+    });
+  });
+});

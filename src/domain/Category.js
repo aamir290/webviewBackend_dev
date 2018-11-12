0e123679c12f65c5a0e39b117752307dd90fe483
Jeremy Le Piolet <jeremy.le-piolet@gfi.fr>
2018-11-12 16:30:58 +0100

    Add - Category item

---
+++ src/domain/Category.js
@@ -0,0 +1,13 @@
+/**
+ * Represent a category for chatbots.
+ */
+class Category {
+
+  /**
+   * Create new category
+   * @param name category's name
+   */
+  constructor(name){
+    this.name = name;
+  }
+}

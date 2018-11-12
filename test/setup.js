ce908c9c28e247b6959b1f7a34d64eed34c11ac2
Jeremy Le Piolet <jeremy.le-piolet@gfi.fr>
2018-11-12 12:11:05 +0100

    Add - Application test
    Add sinon for spying on var

--- test/setup.js
+++ test/setup.js
@@ -2,4 +2,7 @@
  * Common setup for tests
  */
 
+const chai = require('chai');
+const sinonChai = require('sinon-chai');
 
+chai.use(sinonChai);
---
+++ test/unit/infrastructure/Application.spec.js
@@ -0,0 +1,28 @@
+/**
+ * Unit tests for Application
+ */
+const {expect} = require('chai');
+const sinon = require('sinon');
+const Application = require('../../../src/infrastructure/Application');
+
+describe('Application', () => {
+
+  context('when launching application', () => {
+    it('call server start once', (done) => {
+      const mockServer = {};
+      mockServer.start = sinon.spy();
+      const application = new Application(mockServer);
+
+      application.start();
+
+      expect(mockServer.start).to.have.been.calledOnce;
+      done();
+    });
+  });
+
+
+  afterEach(() => {
+    // Restore the default sandbox here
+    sinon.restore();
+  });
+});

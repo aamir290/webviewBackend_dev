9df4f1b034d571c956e8ea658f4a892b0700d459
Jeremy Le Piolet <jeremy.le-piolet@gfi.fr>
2018-11-12 10:59:57 +0100

    Add - unit test for UseCase class

---
+++ test/unit/usecases/UseCaseTest.spec.js
@@ -0,0 +1,31 @@
+/**
+ * Unit tests for GetAllCategoriesUseCase
+ */
+const {expect} = require('chai');
+const UseCase = require('../../../src/usecases/UseCase');
+
+describe('UseCase', () => {
+
+  context('when adding callback to events', () => {
+    let useCase;
+
+    before(() => {
+      useCase = new UseCase(['SUCCESS', 'SUCCESS2']);
+    });
+
+    it('add callback to success event', (done) => {
+      useCase.on(useCase.events.SUCCESS, () => {
+      });
+
+      done();
+    });
+
+    it('throws error for unknown event', (done) => {
+      expect(() =>
+        useCase.on('HELLO', () => {
+        }))
+        .to.throw('Event HELLO doesn\'t exists for UseCase');
+      done();
+    });
+  });
+});

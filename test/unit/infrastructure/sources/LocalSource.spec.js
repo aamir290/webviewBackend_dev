371a47414a4c2e4b824fea380cd94474b3f9cf45
Jeremy Le Piolet <jeremy.le-piolet@gfi.fr>
2018-11-13 12:03:18 +0100

    Change - add test for incorrect path in datasource

--- test/unit/infrastructure/sources/LocalSource.spec.js
+++ test/unit/infrastructure/sources/LocalSource.spec.js
@@ -1,6 +1,7 @@
 /**
  * Unit tests for LocalSource
  */
+const sinon = require('sinon');
 const LocalSource = require('../../../../src/infrastructure/sources/LocalSource');
 const testDataPath = './test/unit/testData';
 
@@ -22,6 +23,17 @@ describe('LocalSource', () => {
           name: 'Finance'
         }
       ], 'Array categories should be the same');
+    });
+
+    it('return empty array if path is incorrect', async () => {
+      const stubLogger = {};
+      stubLogger.error = sinon.stub();
+
+      const localSource = new LocalSource('titi', stubLogger);
+      const categories = await localSource.getRootCategories();
+
+      categories.length.should.be.equal(0);
+      stubLogger.error.should.have.been.calledTwice;
     });
   });
 });

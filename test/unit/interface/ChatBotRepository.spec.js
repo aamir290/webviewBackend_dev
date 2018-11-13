74520df14a5f4929186f36d6aa751a7521f05584
Jeremy Le Piolet <jeremy.le-piolet@gfi.fr>
2018-11-13 15:48:07 +0100

    Add - test on chatbotrepository when problemes occurs with local source

--- test/unit/interface/ChatBotRepository.spec.js
+++ test/unit/interface/ChatBotRepository.spec.js
@@ -1,6 +1,7 @@
 /**
  * Unit tests for chatBotReoository
  */
+const {expect} = require('chai');
 const sinon = require('sinon');
 const ChatBotRepository = require('../../../src/interface/ChatBotRepository');
 const LocalSource = require('../../../src/infrastructure/sources/LocalSource');
@@ -20,6 +21,35 @@ describe('ChatBotRepository', () => {
       stubGetAllCategoriesLocalSource.should.have.been.calledOnce;
       categories.should.eql([], 'Array categories should be the same');
 
+    });
+
+    it('return array when local sources has  data', async () => {
+      const stubGetAllCategoriesLocalSource = sinon.stub().resolves([{
+        id: 'educ',
+        name: 'Education'
+      }]);
+      const stubLocalSource = sinon.createStubInstance(LocalSource, {
+        getRootCategories: stubGetAllCategoriesLocalSource
+      });
+
+      const chatBotRepository = new ChatBotRepository(stubLocalSource);
+      const categories = await chatBotRepository.getRootCategories();
+
+      stubGetAllCategoriesLocalSource.should.have.been.calledOnce;
+      categories.should.eql([{
+        id: 'educ',
+        name: 'Education'
+      }], 'Array categories should be the same');
+
+    });
+
+    it('return undefined when local source has a problem', async () => {
+      const stubLocalSource = sinon.createStubInstance(LocalSource);
+
+      const chatBotRepository = new ChatBotRepository(stubLocalSource);
+      const categories = await chatBotRepository.getRootCategories();
+
+      expect(categories).to.eql(undefined);
     });
   });
 

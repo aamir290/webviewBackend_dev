18fbe243844c935dd31cda7216183ce3ad13b138
Jeremy Le Piolet <jeremy.le-piolet@gfi.fr>
2018-11-14 10:41:21 +0100

    Add - add test for api router

---
+++ test/unit/infrastructure/routes/apiRouter.spec.js
@@ -0,0 +1,51 @@
+/**
+ * Unit tests for apiRouter
+ */
+const sinon = require('sinon');
+const request = require('supertest');
+const express = require('express');
+const ApiRouter = require('../../../../src/infrastructure/routes/ApiRouter');
+const GetRootCategoriesUseCase = require('../../../../src/usecases/GetRootCategoriesUseCase');
+const ChatBotRepository = require('../../../../src/interface/ChatBotRepository');
+
+
+describe('GET /api', function() {
+
+  it('respond with json with categories', function(done) {
+    //Init
+    const stubChatBotRepository = sinon.createStubInstance(ChatBotRepository, {
+      getRootCategories: sinon.stub().resolves([
+        {
+          id: 'educ',
+          name: 'Education'
+        }
+      ]),
+    });
+    const getRootCategoriesUseCaseImpl = new GetRootCategoriesUseCase(stubChatBotRepository);
+    const useCaseContainer = {};
+    useCaseContainer.getRootCategoriesUsecase = getRootCategoriesUseCaseImpl;
+
+
+    const apiRouter = new ApiRouter(useCaseContainer, {}, {});
+    const app = express();
+    app.use(apiRouter.apiRouter);
+
+    //Test
+    request(app)
+      .get('/api')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .then(response => {
+        response.body.should.eql([
+          {
+            id: 'educ',
+            name: 'Education'
+          }
+        ]);
+        done();
+      })
+      .catch((e)=>{
+        done(e);
+      });
+  });
+});

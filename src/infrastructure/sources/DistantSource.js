732106ce8722453cfec7febe65ef611f3736915c
Jeremy Le Piolet <jeremy.le-piolet@gfi.fr>
2018-11-14 16:40:23 +0100

    Add - create empty DistantSource class

---
+++ src/infrastructure/sources/DistantSource.js
@@ -0,0 +1,28 @@
+/**
+ * Return data from distant ChatBot Directory server
+ */
+
+class DistantSource {
+
+  /**
+   * Initiate distant source
+   */
+  constructor(){
+
+  }
+
+  /**
+   * Return array of chatbots for given category
+   * Empty array if no chatbots for given category
+   * @param categoryId id of category to retrieve chatbots from
+   * @returns categories array
+   */
+  listCategory(categoryId){
+    const chatbots = [];
+
+    return chatbots;
+  }
+
+}
+
+module.exports = DistantSource;

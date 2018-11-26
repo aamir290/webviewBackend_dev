4df14494afc3fafb301336c8bd080af8ee2f4fd5
Jeremy Le Piolet <jeremy.le-piolet@gfi.fr>
2018-11-26 10:46:58 +0100

    Add - middleware to check category id parameter
    Use validator.js to sanitize and validate parameter

--- src/infrastructure/validation/categoryParameterMiddleware.js
+++ src/infrastructure/validation/categoryParameterMiddleware.js
@@ -1,8 +1,41 @@
 /**
  * Middleware that handle validation of category parameter in request.
+ * Sanitize parameter to
  */
 
+const validator = require('validator');
+const Status = require('http-status');
+
 module.exports = function (req, res, next) {
-  console.log('LOGGED');
-  next();
+  if(req && req.params && req.params.categoryId){ //Parameter exists
+    let paramCategoryId = req.params.categoryId;
+
+    paramCategoryId = validator.trim(paramCategoryId);  //remove unused space
+    paramCategoryId = validator.escape(paramCategoryId);  //convert html char to html entities
+
+    if(!validator.isAlpha(paramCategoryId)) {//only alpha char
+      _sendBadRequest(res);
+    }
+
+    if(!validator.isLength(paramCategoryId, {min:4, max:8})) {//category name between 4 & 8
+      _sendBadRequest(res);
+    }
+
+    req.params.categoryId = paramCategoryId;  //set sanitize param
+
+    next();
+  }else{
+    _sendBadRequest(res);
+  }
 };
+
+/**
+ * Return response 400 for bad request.
+ * @param res
+ * @private
+ */
+function _sendBadRequest(res) {
+  res
+    .status(Status.BAD_REQUEST)
+    .end();
+}
---
+++ test/unit/infrastructure/validation/categoryParameterMiddleware.spec.js
@@ -0,0 +1,84 @@
+/**
+ * Unit tests for Server
+ */
+
+const sinon = require('sinon');
+const categoryParameterMiddleware = require('../../../../src/infrastructure/validation/categoryParameterMiddleware');
+
+describe('categoryParameterMiddleware', () => {
+
+  let stubRequest, stubResponse, stubNext;
+
+  before(() => {
+    stubRequest = {};
+    stubRequest.params = {};
+
+    stubResponse = {};
+    stubResponse.status = sinon.stub();
+    stubResponse.status.returns(stubResponse);
+    stubResponse.end = sinon.stub();
+
+    stubNext = sinon.stub();
+  });
+
+  context('when testing correct category id', () => {
+    it('call next with category with 4 chars',  () => {
+      stubRequest.params.categoryId = 'fina';
+
+      categoryParameterMiddleware(stubRequest, stubResponse, stubNext);
+
+      stubNext.should.have.been.calledOnce;
+    });
+
+    it('call next with category with 8 chars',  () => {
+      stubRequest.params.categoryId = 'finabank';
+
+      categoryParameterMiddleware(stubRequest, stubResponse, stubNext);
+
+      stubNext.should.have.been.calledOnce;
+    });
+  });
+
+  context('when testing incorrect category id', () => {
+    it('return 400 BAD Request with category < 4 chars',  () => {
+      stubRequest.params.categoryId = 'fin';
+
+      categoryParameterMiddleware(stubRequest, stubResponse, stubNext);
+
+      stubResponse.status.should.have.been.calledOnceWith(400);
+      stubResponse.end.should.have.been.calledOnce;
+    });
+
+    it('return 400 BAD Request with category > 8 chars',  () => {
+      stubRequest.params.categoryId = 'finabanke';
+
+      categoryParameterMiddleware(stubRequest, stubResponse, stubNext);
+
+      stubResponse.status.should.have.been.calledOnceWith(400);
+      stubResponse.end.should.have.been.calledOnce;
+    });
+
+    it('return 400 BAD Request with category with num',  () => {
+      stubRequest.params.categoryId = 'fina12z';
+
+      categoryParameterMiddleware(stubRequest, stubResponse, stubNext);
+
+      stubResponse.status.should.have.been.calledWith(400);
+      stubResponse.end.should.have.been.called;
+    });
+
+    it('return 400 BAD Request with category incorrect char',  () => {
+      stubRequest.params.categoryId = 'finre<e';
+
+      categoryParameterMiddleware(stubRequest, stubResponse, stubNext);
+
+      stubResponse.status.should.have.been.calledWith(400);
+      stubResponse.end.should.have.been.called;
+    });
+  });
+
+  afterEach(()=>{
+    sinon.resetHistory();
+  });
+
+});

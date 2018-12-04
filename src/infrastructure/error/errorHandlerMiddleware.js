476488b752fea4e206ce680b932950dd73fd44c8
Jeremy Le Piolet <jeremy.le-piolet@gfi.fr>
2018-12-04 10:45:09 +0100

    Change - return status and message on error handler

--- src/infrastructure/error/errorHandlerMiddleware.js
+++ src/infrastructure/error/errorHandlerMiddleware.js
@@ -1,6 +1,18 @@
-
+/**
+ * Handle http error.
+ * If not http error, call next error handler.
+ * Id dev mod, display error message. Not in production mode.
+ * @param err
+ * @param req
+ * @param res
+ * @param next
+ */
 module.exports = (err, req, res, next) => {
-
-  console.log('Coucou');
-  next();
+  if (err.status) {
+    res
+      .status(err.status)
+      .end(err.message);
+  } else {
+    next(err);
+  }
 };

88cc4ed4886822b45ef664631175c67220983a2b
Jeremy Le Piolet <jeremy.le-piolet@gfi.fr>
2018-12-04 15:11:37 +0100

    Change - refactroing categoryParameterMiddleware to be sure only one next call done each time

--- src/infrastructure/validation/categoryParameterMiddleware.js
+++ src/infrastructure/validation/categoryParameterMiddleware.js
@@ -15,15 +15,12 @@ module.exports = function (req, res, next) {
 
     if(!validator.isAlpha(paramCategoryId)) {//only alpha char
       next(new HTTPError(400, 'Invalid Category Id - not alpha only'));
-    }
-
-    if(!validator.isLength(paramCategoryId, {min:4, max:8})) {//category name between 4 & 8
+    }else if(!validator.isLength(paramCategoryId, {min:4, max:8})) {//category name between 4 & 8
       next(new HTTPError(400, 'Invalid Category Id - id length must be between 4 and 8'));
+    }else {
+      req.params.categoryId = paramCategoryId;  //set sanitize param
+      next();
     }
-
-    req.params.categoryId = paramCategoryId;  //set sanitize param
-
-    next();
   }else{
     next(new HTTPError(400, 'Invalid Category Id - Missing parameter'));
   }

201fd95cf9ca5eadbe7a826453204070ce596533
Marie <marie.delavalette@gfi.fr>
2018-12-19 17:19:19 +0100

    ADD beginInteraction usecase

---
+++ src/usecases/BeginInteractionUseCase.js
@@ -0,0 +1,50 @@
+const UseCase = require('./UseCase');
+
+/**
+ * Get deeplink to trigger conversation with boot
+ * Launch request to chatbotrepository and return Intent/Deeplink list, if exists.
+ * Extends EventEmitter to handle different response for method :
+ *    - SUCCESS => response OK
+ *    - ERROR => error in process
+ *    - PARAMETER_ERROR => error with ID parameter (+ error message)
+ */
+class BeginInteractionUseCase extends UseCase {
+
+  constructor(chatBotRepository, logger) {
+    super(['SUCCESS', 'ERROR', 'PARAMETER_ERROR']);
+
+    this.chatBotRepository = chatBotRepository;
+
+    if (!logger) throw new Error('Missing logger');
+    this.logger = logger;
+  }
+
+  /**
+   * Launch use case task.
+   * @param chatbotId chatbotId to contact
+   * @param MSISDN user number phone
+   *  @param accessChannel access channel
+   */
+  async execute(chatbotId, MSISDN, accessChannel) {
+    const {SUCCESS, NOT_FOUND, PARAMETER_ERROR} = this.events;
+    if(chatbotId) {
+      try {
+        const ABCDE_id = await this.chatBotRepository.getChatBotId(chatbotId);
+        this.logger.debug('BeginInteraction - getChatBotId return : ' + ABCDE_id);
+
+        if (ABCDE_id) {
+          this.emit(SUCCESS, ABCDE_id);
+        } else {
+          this.emit(NOT_FOUND);
+        }
+      } catch (e) {
+        this.logger.debug('BeginInteraction - catch : ' + e);
+        this.emit(PARAMETER_ERROR, 'Incorrect getChatBotId');
+      }
+    }else {
+      this.emit(PARAMETER_ERROR, 'Incorrect chatbotId parameter');
+    }
+  }
+}
+
+module.exports = BeginInteractionUseCase;

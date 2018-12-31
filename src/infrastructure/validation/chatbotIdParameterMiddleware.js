511bc4868ed953ad61aedd0c0ef476da616f94d0
Marie <marie.delavalette@gfi.fr>
2018-12-31 16:03:23 +0100

    Add - Chatbot Id validation testing

--- src/infrastructure/validation/chatbotIdParameterMiddleware.js
+++ src/infrastructure/validation/chatbotIdParameterMiddleware.js
@@ -14,13 +14,15 @@ module.exports = function (req, res, next) {
   if(req && req.params && req.params.chatbotId){ //Parameter exists
     let paramCategoryId = validator.trim(req.params.chatbotId);  //remove unused space
 
-    if(!validator.isLength(paramCategoryId, {min:6, max:60})) {//chatbotId between 6 & 60
-      next(new HTTPError(400, 'Invalid Chatbot Id - id length must be between 6 and 60'));
+    if(paramCategoryId.indexOf(' ') !== -1) {//only one word
+      next(new HTTPError(400, 'Invalid chatbot id - must be one word'));
+    }else if(!validator.isLength(paramCategoryId, {min:6, max:60})) {//chatbotId between 6 & 60 chars
+      next(new HTTPError(400, 'Invalid chatbot id - id length must be between 6 and 60'));
     }else {
       req.params.chatbotId = paramCategoryId;  //set sanitize param
       next();
     }
   }else{
-    next(new HTTPError(400, 'Invalid Chatbot Id - Missing parameter'));
+    next(new HTTPError(400, 'Invalid chatbot id - missing parameter'));
   }
 };
